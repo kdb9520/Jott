8cbf4c6a33ad15483b828370adf87784d7ca3f0e
Derek Pruski <dmp3872@g.rit.edu>
2023-10-03 11:43:14 -0400

    Function Call Node

---
+++ jott_translator/src/nodes/FunctionCallNode.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+import provided.JottTree;
+import provided.Token;
+import provided.TokenType;
+
+public class FunctionCallNode implements JottTree {
+    private IDNode funcName;
+    private FuncParamNode params;
+    
+    public FunctionCallNode (IDNode f_name, FuncParamNode f_p) {
+        this.funcName = f_name;
+        this.params = f_p;
+    }
+
+    public boolean validateTree() {
+        // TODO
+        return false;
+    }
+
+    public String convertToJava(String classname) {
+        // TODO
+        return "not implemented";
+    }
+
+    public String convertToPython() {
+        // TODO
+        return "not implemented";
+    }
+
+    public String convertToC() {
+        // TODO
+        return "not implemented";
+    }
+
+    public String convertToJott() {
+        String output = "::";
+        output += funcName.convertToJott();
+        output  += "[";
+        output += params.convertToJott();
+        output += "]";
+        return output;
+    }
+}
