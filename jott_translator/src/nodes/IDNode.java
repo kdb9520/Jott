e9b74349e60a71ae790d02863815f7eb57e9e0bc
JustinC17 <jgc9449@g.rit.edu>
2023-10-03 11:32:16 -0400

    Make Nodes folder

---
+++ jott_translator/src/nodes/IDNode.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+import provided.JottTree;
+import provided.Token;
+import provided.TokenType;
+
+public class IDNode implements JottTree {
+    private Token token;
+    
+    public IDNode (Token t) {
+        this.token = t;
+    }
+
+    public boolean validateTree() {
+        // TODO
+        return false;
+    }
+
+    public String convertToJava(String classname) {
+        // TODO
+        return "not implemented";
+    }
+
+    public String convertToPython() {
+        // TODO
+        return "not implemented";
+    }
+
+    public String convertToC() {
+        // TODO
+        return "not implemented";
+    }
+
+    public String convertToJott() {
+        return this.token.getToken();
+    }
+
+    static public IDNode partseIDNode(ArrayList<Token> tokens) {
+        if (tokens.get(0).getTokenType() != TokenType.ID_KEYWORD) {
+            // throw syntax exception
+        }
+        return new IDNode(tokens.remove(0));
+    }
+}
