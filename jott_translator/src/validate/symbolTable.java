613349691f03136c23e7f7a1bfb7ee3c1eb3ffe4
meo6332 <meo6332@rit.edu>
2023-11-02 11:52:38 -0400

    Made the validate folder

---
+++ jott_translator/src/validate/symbolTable.java
@@ -0,0 +1,11 @@
+package validate;
+
+/**
+ * This class is meant to be used across the nodes to properly validate in phase 3
+ * 
+ * @author Max O'Malley
+ */
+
+public class symbolTable {
+    
+}
