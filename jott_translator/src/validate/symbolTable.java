a16bf9dddb381ba7fef249bc9879b9c1e93cb5c4
meo6332 <meo6332@rit.edu>
2023-11-02 11:57:23 -0400

    Add extra notes to symbolTable

--- jott_translator/src/validate/symbolTable.java
+++ jott_translator/src/validate/symbolTable.java
@@ -7,5 +7,10 @@ package validate;
  */
 
 public class symbolTable {
-    
+    // Have a current function value 
+    // That current function value is set by the FuncDefNode
+    // So then when a variable is added to the symbol table, regardless of where it's 
+    // called from it'll know what row of the symbol table to add it to. 
+
+    // Have a symbol table add based on the current 
 }
