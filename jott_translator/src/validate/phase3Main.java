68a2856788b705fa937897295fbe61b7f03f3ae2
meo6332 <meo6332@rit.edu>
2023-11-09 11:38:45 -0500

    Stubbed out phase 3 main

---
+++ jott_translator/src/validate/phase3Main.java
@@ -0,0 +1,23 @@
+package validate;
+
+import provided.*;
+import nodes.*;
+
+public class phase3Main {
+    
+    
+    public static void processJott(String jottFilename){
+        // This needs to somehow run all the phases up to this point.
+        // Tokenize (call tokenizer)
+        // Parse (call parser)
+        // Run validateTree function
+        // In phase 4 we can have another param that is the langauge to write to. 
+        // For now treat it as Jott always
+
+    }
+
+
+    public static void main(){
+        // This will deal with .io and then call processJott
+    }
+}
