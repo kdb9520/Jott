67f1622570240a513d661946d751079171b90c89
Kellen Bell <93352579+kdb9520@users.noreply.github.com>
2023-12-06 11:09:01 -0500

    args for run.bash

--- run.bash
+++ run.bash
@@ -9,8 +9,26 @@ do
     IFS='.'
     read -ra dotarr <<< "${splitarr[-1]}"
 
-    java phase4Main ../../../phase4Tests/${dotarr[0]}.jott ../../../output/c/${dotarr[0]}.c C
-    java phase4Main ../../../phase4Tests/${dotarr[0]}.jott ../../../output/java/${dotarr[0]}.java Java
-    java phase4Main ../../../phase4Tests/${dotarr[0]}.jott ../../../output/python/${dotarr[0]}.py Python
+    if [[ $1 == "c" ]]
+    then
+        java phase4Main ../../../phase4Tests/${dotarr[0]}.jott ../../../output/c/${dotarr[0]}.c C
+    fi
+
+    if [[ $1 == "j" ]]
+    then
+        java phase4Main ../../../phase4Tests/${dotarr[0]}.jott ../../../output/java/${dotarr[0]}.java Java
+    fi
+
+    if [[ $1 == "p" ]]
+    then
+        java phase4Main ../../../phase4Tests/${dotarr[0]}.jott ../../../output/python/${dotarr[0]}.py Python
+    fi
+
+    if [[ -z $1 ]]
+    then
+        java phase4Main ../../../phase4Tests/${dotarr[0]}.jott ../../../output/c/${dotarr[0]}.c C
+        java phase4Main ../../../phase4Tests/${dotarr[0]}.jott ../../../output/java/${dotarr[0]}.java Java
+        java phase4Main ../../../phase4Tests/${dotarr[0]}.jott ../../../output/python/${dotarr[0]}.py Python
+    fi
 
 done
