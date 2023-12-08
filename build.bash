beb2829a02f5cb9a0c39cc86434f814dfd6fe0a4
Kellen Bell <93352579+kdb9520@users.noreply.github.com>
2023-12-07 20:18:24 -0500

    compile the validate

--- build.bash
+++ build.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd jott_translator/src
-javac nodes/*.java provided/*.java phase4Main.java -d classes
+javac nodes/*.java provided/*.java validate/*.java phase4Main.java -d classes
 cd ..
 cd ..
